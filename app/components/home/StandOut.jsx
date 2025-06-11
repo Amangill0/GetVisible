148ea9fc0604942c0a20e0b7f1c614bc5a74da55
amangill0 <amangill9996@gmail.com>
2025-06-11 11:45:29 +0530

    padding

--- app/components/home/StandOut.jsx
+++ app/components/home/StandOut.jsx
@@ -1,13 +1,13 @@
-import React from 'react'
+import React from "react";
 
 const StandOut = () => {
   return (
     <div className=" bg-[url(/images/standout.png)] -mt-30 bg-center px-4  bg-no-repeat bg-cover bg ">
-      <h2 className=" text-white text-center xl:pt-25 pt-30   pl-20 sm:pb-80 pb-20  md:pb-100 lg:pb-115 text-xl  sm:text-4xl lg:text-4xl  xl:text-[72.728px] mont font-black  ">
+      <h2 className=" text-white text-center xl:pt-25 max-md:pt-40 md:pt-30  max-sm:text-center  md:pl-20 sm:pb-80 pb-20  md:pb-100 lg:pb-115 text-xl  sm:text-4xl lg:text-4xl  xl:text-[72.728px] mont font-black  ">
         STAND OUT FROM THE <span className="italic">CROWD</span>
       </h2>
     </div>
   );
-}
+};
 
-export default StandOut
+export default StandOut;
